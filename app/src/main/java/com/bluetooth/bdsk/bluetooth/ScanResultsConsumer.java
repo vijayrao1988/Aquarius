52aa1d8b711242aa97b8c79634cc74742d69d836
vijayrao1988 <vijayrao1988@gmail.com>
2018-04-16 16:33:57 +0530

    Aquarius changes over bdsk app

--- app/src/main/java/com/bluetooth/bdsk/bluetooth/ScanResultsConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.bluetooth.bdsk.bluetooth;
-import android.bluetooth.BluetoothDevice;
-public interface ScanResultsConsumer {
-    public void candidateBleDevice(BluetoothDevice device, byte[] scan_record, int rssi);
-    public void scanningStarted();
-    public void scanningStopped();
-}
